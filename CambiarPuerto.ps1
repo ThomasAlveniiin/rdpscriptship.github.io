0bada5ee01fcca8ee4171bbd089a48f89fbc065b
ThomasAlveniiin <116445576+ThomasAlveniiin@users.noreply.github.com>
2025-01-18 16:43:09 +0100

    Update CambiarPuerto.ps1

--- CambiarPuerto.ps1
+++ CambiarPuerto.ps1
@@ -1,15 +1,5 @@
-# Leer dominio como parámetro opcional
-param (
-    [string]$Dominio = $(Read-Host "Introduce el dominio de tu web")
-)
-
-# Mostrar el dominio ingresado
-Write-Host "El dominio ingresado es: $Dominio"
-
-# Cambiar el valor del puerto en el registro
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -Name "PortNumber" -Value 12000 -Type DWord
 
-# Reiniciar el servicio de Terminal Services
 Write-Host "Reiniciando el servicio de Terminal Services..."
 net stop termservice
 net start termservice
