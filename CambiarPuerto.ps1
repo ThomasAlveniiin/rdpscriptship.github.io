6969bc3d3aee0b882fa984daff2bb4cf80f6201b
ThomasAlveniiin <116445576+ThomasAlveniiin@users.noreply.github.com>
2025-01-18 16:51:48 +0100

    Update CambiarPuerto.ps1

--- CambiarPuerto.ps1
+++ CambiarPuerto.ps1
@@ -4,4 +4,9 @@ Write-Host "Reiniciando el servicio de Terminal Services..."
 net stop termservice
 net start termservice
 
-Write-Host "El cambio de puerto se ha completado y el servicio se ha reiniciado."
+New-NetFirewallRule -DisplayName "Permitir puerto 12000 TCP" -Direction Inbound -Protocol TCP -LocalPort 12000 -Action Allow
+New-NetFirewallRule -DisplayName "Permitir puerto 12000 UDP" -Direction Inbound -Protocol UDP -LocalPort 12000 -Action Allow
+New-NetFirewallRule -DisplayName "Permitir puerto 12000 TCP" -Direction Outbound -Protocol TCP -LocalPort 12000 -Action Allow
+New-NetFirewallRule -DisplayName "Permitir puerto 12000 UDP" -Direction Outbound -Protocol UDP -LocalPort 12000 -Action Allow
+
+Write-Host "El cambio de puerto y las reglas de firewall se han completado y el servicio se ha reiniciado."
